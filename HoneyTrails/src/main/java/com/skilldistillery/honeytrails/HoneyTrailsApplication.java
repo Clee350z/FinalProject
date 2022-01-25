c40280e39219b28955de94e13166288403b333e3
Chris Lee <91095529+Clee350z@users.noreply.github.com>
2022-01-25 11:51:11 -0700

    Project Set up

---
+++ HoneyTrails/src/main/java/com/skilldistillery/honeytrails/HoneyTrailsApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.honeytrails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoneyTrailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoneyTrailsApplication.class, args);
+	}
+
+}
