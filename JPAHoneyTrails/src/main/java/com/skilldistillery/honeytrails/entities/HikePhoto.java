06059b9310768c8b3a588371cacd71577effbb8d
Ebony Johnson <ejohnson23451@gmail.com>
2022-01-26 13:00:09 -0500

    Added Entities Condition, Hike Report, Hike Photo

---
+++ JPAHoneyTrails/src/main/java/com/skilldistillery/honeytrails/entities/HikePhoto.java
@@ -0,0 +1,9 @@
+package com.skilldistillery.honeytrails.entities;
+
+public class HikePhoto {
+	
+	private int id;
+	
+	private String imageUrl;
+
+}
---
+++ JPAHoneyTrails/src/main/java/com/skilldistillery/honeytrails/entities/HikeReport.java
@@ -0,0 +1,5 @@
+package com.skilldistillery.honeytrails.entities;
+
+public class HikeReport {
+
+}
