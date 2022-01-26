8f15986db80d3dce7799a0d9d85b4fe410edca13
Chris Lee <91095529+Clee350z@users.noreply.github.com>
2022-01-26 10:29:43 -0700

    Updated User with fav trail mapping and add address

--- JPAHoneyTrails/src/main/java/com/skilldistillery/honeytrails/entities/User.java
+++ JPAHoneyTrails/src/main/java/com/skilldistillery/honeytrails/entities/User.java
@@ -1,11 +1,18 @@
 package com.skilldistillery.honeytrails.entities;
 
+import java.util.List;
 import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 @Entity
 public class User {
@@ -18,10 +25,29 @@ public class User {
 
 	private String password;
 
-	private Boolean enabled;
+	@Column(name="profile_picture")
+	private String profilePicture;
 
 	private String role;
 
+	private Boolean enabled;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	//need to make "user" in address
+	@OneToMany(mappedBy="user")
+	private Integer addressId;
+	
+	private String biography;
+	
+	@ManyToMany(fetch=FetchType.EAGER)
+	@JoinTable(name="favorite_trail", 
+			joinColumns=@JoinColumn(name="user_id"),
+			inverseJoinColumns=@JoinColumn(name="trail_id"))
+	private List<Trail> favoriteTrails;
+	
 	public User() {
 		super();
 	}
@@ -65,6 +91,46 @@ public class User {
 	public void setRole(String role) {
 		this.role = role;
 	}
+	
+	public String getProfilePicture() {
+		return profilePicture;
+	}
+
+	public void setProfilePicture(String profilePicture) {
+		this.profilePicture = profilePicture;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Integer getAddressId() {
+		return addressId;
+	}
+
+	public void setAddressId(Integer addressId) {
+		this.addressId = addressId;
+	}
+
+	public String getBiography() {
+		return biography;
+	}
+
+	public void setBiography(String biography) {
+		this.biography = biography;
+	}
 
 	@Override
 	public int hashCode() {
@@ -85,8 +151,9 @@ public class User {
 
 	@Override
 	public String toString() {
-		return "User [id=" + id + ", username=" + username + ", password=" + password + ", enabled=" + enabled
-				+ ", role=" + role + "]";
+		return "User [id=" + id + ", username=" + username + ", password=" + password + ", profilePicture="
+				+ profilePicture + ", role=" + role + ", enabled=" + enabled + ", firstName=" + firstName
+				+ ", lastName=" + lastName + ", addressId=" + addressId + ", biography=" + biography + "]";
 	}
 
 }
