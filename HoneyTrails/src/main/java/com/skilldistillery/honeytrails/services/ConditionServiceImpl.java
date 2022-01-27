ffc4f5c3c673989cb33400bb53ec8fbeaa941c9c
Ebony Johnson <ejohnson23451@gmail.com>
2022-01-27 15:09:06 -0500

    Update Condition Service

--- HoneyTrails/src/main/java/com/skilldistillery/honeytrails/services/ConditionServiceImpl.java
+++ HoneyTrails/src/main/java/com/skilldistillery/honeytrails/services/ConditionServiceImpl.java
@@ -1,5 +1,20 @@
 package com.skilldistillery.honeytrails.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.skilldistillery.honeytrails.entities.Condition;
+import com.skilldistillery.honeytrails.repositories.ConditionRepository;
+
 public class ConditionServiceImpl implements ConditionService {
+
+	@Autowired
+	private ConditionRepository condRepo;
+	
+	@Override
+	public List<Condition> index() {
+		return condRepo.findAll();
+	}
 
 }
