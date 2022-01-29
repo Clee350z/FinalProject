db8930a42a40458c981984c84626e739fe6f4c3b
Ryan Majors <growbox.ryan@yahoo.com>
2022-01-29 11:59:12 -0700

    Created home,navigation,not-found, and register components

--- ngHoneyTrails/src/app/app.module.ts
+++ ngHoneyTrails/src/app/app.module.ts
@@ -10,6 +10,10 @@ import { UserComponent } from './components/user/user.component';
 import { HikePhotoComponent } from './components/hike-photo/hike-photo.component';
 import { HikeReportComponent } from './components/hike-report/hike-report.component';
 import { HikeReportCommentComponent } from './components/hike-report-comment/hike-report-comment.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +22,11 @@ import { HikeReportCommentComponent } from './components/hike-report-comment/hik
     UserComponent,
     HikePhotoComponent,
     HikeReportComponent,
-    HikeReportCommentComponent
+    HikeReportCommentComponent,
+    HomeComponent,
+    NavigationComponent,
+    NotFoundComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
