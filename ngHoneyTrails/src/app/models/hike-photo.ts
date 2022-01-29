aaa778ef35b64d234ffdc91bbb086273b51ad3f8
Ebony Johnson <ejohnson23451@gmail.com>
2022-01-29 13:47:06 -0500

    Finished model creation for Condition, Hike Report, Hike Report Comment, & Hike Photo

--- ngHoneyTrails/src/app/models/hike-photo.ts
+++ ngHoneyTrails/src/app/models/hike-photo.ts
@@ -1,2 +1,23 @@
+import { HikeReport } from "./hike-report";
+
 export class HikePhoto {
+  id: number;
+  hikeReport: HikeReport | undefined;
+  imageUrl: string | undefined;
+  title: string | undefined;
+  description: string | undefined;
+
+  constructor(
+    id: number = 0,
+    hikeReport?: HikeReport,
+    imageUrl?: string,
+    title?: string,
+    description?: string
+  ){
+    this.id = id;
+    this.hikeReport = hikeReport;
+    this.imageUrl = imageUrl;
+    this.title = title;
+    this.description = description;
+  }
 }
--- ngHoneyTrails/src/app/models/hike-report.ts
+++ ngHoneyTrails/src/app/models/hike-report.ts
@@ -1,10 +1,37 @@
 import { Condition } from "./condition";
 import { Trail } from "./trail";
+import { User } from "./user";
 
 export class HikeReport {
 id: number;
 hikeTitle: string | undefined;
 trails: Trail | undefined;
 condition: Condition | undefined;
+report: string | undefined;
+dateCreated: string | undefined;
+hikedDate: string | undefined;
+user: User | undefined;
+rating: number | undefined;
 
+constructor(
+id: number = 0,
+hikeTitle?: string,
+trails?: Trail,
+condition?: Condition,
+report?: string,
+dateCreated?: string ,
+hikedDate?: string,
+user?: User,
+rating?: number
+){
+this.id = id;
+this.hikeTitle = hikeTitle;
+this.trails = trails;
+this.condition = condition;
+this.report = report;
+this.dateCreated = dateCreated;
+this.hikedDate = hikedDate;
+this.user = user;
+this.rating = rating;
+}
 }
