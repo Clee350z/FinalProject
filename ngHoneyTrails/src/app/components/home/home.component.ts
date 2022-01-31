9f1c0692e0591120bd5cc58affc74e2a937e5038
Daniel Faulkner <danfaulkner0@gmail.com>
2022-01-31 16:13:05 -0700

    merge stuff

--- ngHoneyTrails/src/app/components/home/home.component.ts
+++ ngHoneyTrails/src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Trail } from 'src/app/models/trail';
+import { TrailService } from 'src/app/services/trail.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  trails : Trail[] = [];
+
+  constructor(
+    private trailSvc : TrailService
+  ) { }
 
   ngOnInit(): void {
+    this.reload();
+  }
+
+  reload(){
+    this.trailSvc.index().subscribe(
+      trails => {
+        this.trails = trails;
+      },
+
+      fail =>{
+        console.error('TrailComponent.reload(): Error retreiving trails');
+        console.error(fail);
+      }
+    );
   }
 
 }
